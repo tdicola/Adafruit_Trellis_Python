f5f8d9436decfcf39d3af9512663677e84e4e026
Tony DiCola <tony@tonydicola.com>
2014-02-11 12:45:02 -0800

    Fix tabs

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
 setup(name='Adafruit_Trellis',
-	  version='1.0',
-	  description='Python port of the Adafruit Trellis library.',
-	  author='Tony DiCola',
-      author_email='tony@tonydicola.com',
-      license='MIT',
-      url='https://github.com/tdicola/Adafruit_Trellis_Python'
-	  py_modules=['Adafruit_Trellis']
-	  )
+	version='1.0',
+	description='Python port of the Adafruit Trellis library.',
+	author='Tony DiCola',
+	author_email='tony@tonydicola.com',
+	license='MIT',
+	url='https://github.com/tdicola/Adafruit_Trellis_Python',
+	py_modules=['Adafruit_Trellis']
+	)
